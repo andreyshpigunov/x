7f69aa8f4fe02caf34a49324a7915fce29cc93da
Andrey Shpigunov <andreyshpigunov@me.com>
2026-04-13 22:41:06 +0300

    0.4.0

--- jest.config.mjs
+++
@@ -1,7 +0,0 @@
-// jest.config.mjs
-export default {
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-  testEnvironment: 'jsdom'
-};
--- src/js/app.js
+++ src/js/app.js
@@ -9,8 +9,8 @@
 
 import './x.js';
 // import Alpine from 'alpinejs'
- 
- 
+
+
 // window.Alpine = Alpine;
 // Alpine.start();
 
@@ -19,26 +19,26 @@ x.init();
 // Animation global functions
 window.element1 = (params) => {
   let rotate = params.progress * -360;
-  params.element.style.transform = 'rotate(' + rotate +  'deg)';
+  params.element.style.transform = 'rotate(' + rotate + 'deg)';
 }
 window.element2 = (params) => {
   let rotate = params.progress * 180;
-  params.element.style.transform = 'rotate(' + rotate +  'deg)';
+  params.element.style.transform = 'rotate(' + rotate + 'deg)';
 }
 window.element3 = (params) => {
   let rotate = params.progress * 120;
-  params.element.style.transform = 'rotate(' + rotate +  'deg)';
+  params.element.style.transform = 'rotate(' + rotate + 'deg)';
 }
 window.headerAnimation = (params) => {
   // console.log(x.device.size.s);
-  let _height = x.device.size.s ? 100 : 120;
+  let _height = 100;
   // console.log(_height);
   let header = qs('.header');
   let headerLogo = qs('.header-logo img');
   let height = _height - params.progress * _height / 2;
   let scale = 1 - params.progress * 0.5;
   header.style.height = height + 'px';
-  headerLogo.style.transform = 'scale(' + scale +  ')';
+  headerLogo.style.transform = 'scale(' + scale + ')';
   x.lib.switchClass(header, 'header_compact', params.progress == 1);
 }
 
